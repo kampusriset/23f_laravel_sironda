68fcc5dcad445daf84100ddac94f48afbb6a6f8b
iqbalnglames <mi06514@gmail.com>
2025-07-31 23:23:52 +0700

    Memperbaiki fungsi navbar dan style: implementasi responsive toggle, mengubah struktur link, dan menambah register page.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,24 +1,50 @@
  <nav class="navbar">
-            <a href="{{ url('/') }}" class="navbar-brand">
-                <img src="{{ asset('images/logo.png') }}" height="50" alt="Logo SiRonda" class="logo">
-                <span class="app-name">{{ config('app.name', 'SiRonda') }}</span>
-            </a>
-        <ul>
-            <li><a href="{{ url('/') }}">Home</a></li>
-            <li><a href="{{ url('/absen') }}">Absen</a></li>
-            <li><a href="{{ url('/contact') }}">Contact</a></li>
-            <span>|</span>
-            <div class="user-menu">
-                @if (Auth::check())
-                    <span class="user-name">{{ Auth::user()->name }}</span>
-                    <a href="{{ url('/profile') }}">Akun</a>
-                    <form action="{{ route('logout') }}" method="POST" class="inline">
-                        @csrf
-                        <button type="submit" class="logout-button">Logout</button>
-                    </form>
-                @else
-                    <a style="color: white" href="{{ route('login') }}" class="login-button">Login</a>
-                @endif
-            </div>
-        </ul>
-</nav>
+     <a href="{{ url('/') }}" class="navbar-brand">
+         <img src="{{ asset('images/logo.png') }}" height="50" alt="Logo SiRonda" class="logo">
+         <span class="app-name">{{ config('app.name', 'SiRonda') }}</span>
+     </a>
+     <div class="navbar-toggle">
+         <div style="border-radius: 2px; height: 4px; width: 30px; background-color: black;"></div>
+         <div style="border-radius: 2px; height: 4px; width: 30px; background-color: black;"></div>
+         <div style="border-radius: 2px; height: 4px; width: 30px; background-color: black;"></div>
+     </div>
+     <div class="navbar-links">
+         <a href="{{ url('/') }}">Home</a>
+         <a href="{{ url('/absen') }}">Absen</a>
+         <a href="{{ url('/contact') }}">Contact</a>
+         <span></span>
+         <div class="user-menu">
+             @if (Auth::check())
+                 <span class="user-name">{{ Auth::user()->name }}</span>
+                 <a href="{{ url('/profile') }}">Akun</a>
+                 <form action="{{ route('logout') }}" method="POST">
+                     @csrf
+                     <button type="submit" class="logout-button">Logout</button>
+                 </form>
+             @else
+                 <a style="color: white" href="{{ route('login') }}" class="login-button">Login</a>
+                 <a style="color: white" href="{{ route('register') }}" class="login-button">Register</a>
+             @endif
+         </div>
+     </div>
+ </nav>
+ <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const toggleButton = document.querySelector('.navbar-toggle');
+            const navbarLinks = document.querySelector('.navbar-links');
+            
+            toggleButton.addEventListener('click', function() {
+                if (!navbarLinks.classList.contains('active')) {
+                    navbarLinks.classList.add('active');
+                } else {
+                    navbarLinks.classList.remove('active');
+                }
+            });
+
+            document.addEventListener('click', (event) => {
+                if(!event.target.closest('.navbar')) {
+                    navbarLinks.classList.remove('active');
+                }
+            })
+        });
+ </script>
